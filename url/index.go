7a9710d6a5b53d58c06928cf5b9bb80c1b92dfa4
李旭 <lixu@puchigames.com>
2022-06-28 17:20:26 +0800

    封装HTTP请求

--- url/index.go
+++ url/index.go
@@ -1,8 +1,12 @@
 package lurl
 
 import (
+	"crypto/tls"
+	"io/ioutil"
 	"net/url"
 	"strings"
+
+	"github.com/astaxie/beego/httplib"
 )
 
 func UrlEncode(str string) string {
@@ -14,4 +18,73 @@ func UrlEncode(str string) string {
 func UrlDecode(str string) string {
 	decodeurl, _ := url.QueryUnescape(str)
 	return decodeurl
+}
+
+func PostBody(apiurl string, data map[string]interface{}) (content []byte, httpcode int, err error) {
+
+	req := httplib.Post(apiurl)
+
+	if strings.Contains(apiurl, "https://") {
+		req.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
+	}
+
+	req.JSONBody(data)
+
+	resp, err := req.Response()
+
+	if err != nil {
+		return
+	}
+
+	httpcode = resp.StatusCode
+
+	content, err = ioutil.ReadAll(resp.Body)
+
+	return
+}
+
+func PostParam(apiurl string, data map[string]string) (content []byte, httpcode int, err error) {
+
+	req := httplib.Post(apiurl)
+
+	if strings.Contains(apiurl, "https://") {
+		req.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
+	}
+
+	for k, v := range data {
+		req.Param(k, v)
+	}
+
+	resp, err := req.Response()
+
+	if err != nil {
+		return
+	}
+
+	httpcode = resp.StatusCode
+
+	content, err = ioutil.ReadAll(resp.Body)
+
+	return
+}
+
+func Get(apiurl string) (content []byte, httpcode int, err error) {
+
+	req := httplib.Get(apiurl)
+
+	if strings.Contains(apiurl, "https://") {
+		req.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
+	}
+
+	resp, err := req.Response()
+
+	if err != nil {
+		return
+	}
+
+	httpcode = resp.StatusCode
+
+	content, err = ioutil.ReadAll(resp.Body)
+
+	return
 }
